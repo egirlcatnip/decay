5a5d4903bd54900565e6e781846ddb0a9c4f7907
egirlcatnip <egirlcatnip@gmail.com>
2025-01-29 18:40:22 +0100

    Lots of changes

--- decay.h
+++ decay.h
@@ -1,145 +1,456 @@
 #ifndef DECAY_H
 #define DECAY_H
 
+/*==============================================================================
+  Type System
+==============================================================================*/
+#include <float.h>
+#include <inttypes.h>
+#include <stdint.h>
+
+// Fixed-width integer types
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+// Floating-point types
+typedef float f32;
+typedef double f64;
+typedef long double f128;
+
+// Architecture-dependent size types
+typedef unsigned long usize;
+typedef signed long isize;
+
+// Integer limits
+#define I8_MAX 127
+#define I16_MAX 32767
+#define I32_MAX 2147483647
+#define I64_MAX 9223372036854775807LL
+#define I8_MIN (-I8_MAX - 1)
+#define I16_MIN (-I16_MAX - 1)
+#define I32_MIN (-I32_MAX - 1)
+#define I64_MIN (-I64_MAX - 1)
+
+#define U8_MAX 255U
+#define U16_MAX 65535U
+#define U32_MAX 4294967295UL
+#define U64_MAX 18446744073709551615ULL
+
+#define U8_MIN 0U
+#define U16_MIN 0U
+#define U32_MIN 0UL
+#define U64_MIN 0ULL
+
+// Floating-point limits
+#define F32_MAX FLT_MAX
+#define F64_MAX DBL_MAX
+#define F128_MAX LDBL_MAX
+#define F32_MIN FLT_MIN
+#define F64_MIN DBL_MIN
+#define F128_MIN LDBL_MIN
+
+/*==============================================================================
+  Print System
+==============================================================================*/
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void println(const char *format, ...);
+#define DECAY_BUFFER_SIZE 4096
+#define DECAY_MAX_SPEC_LEN 32
 
-void print_string(const char *str);
-void print_int(int value);
-void print_float(double value, int precision);
-void print_hex(int value);
-void print_binary(int value);
-void print_pointer(void *ptr);
+typedef enum {
+  FMT_INVALID,
+  FMT_I8,
+  FMT_I16,
+  FMT_I32,
+  FMT_I64,
+  FMT_U8,
+  FMT_U16,
+  FMT_U32,
+  FMT_U64,
+  FMT_F32,
+  FMT_F64,
+  FMT_F128,
+  FMT_STRING,
+  FMT_PTR_LOWER,
+  FMT_PTR_UPPER,
+  FMT_HEX32_LOWER,
+  FMT_HEX32_UPPER,
+  FMT_HEX64_LOWER,
+  FMT_HEX64_UPPER,
+  FMT_BIN32,
+  FMT_BIN64
+} FormatType;
 
-inline void println(const char *format, ...) {
-  va_list args;
-  va_start(args, format);
+typedef struct {
+  FormatType type;
+  int precision;
+} FormatSpec;
 
-  const char *p = format;
-  while (*p) {
-    if (*p == '{') {
-      const char *start = p;
-      p++;
-      if (*p == '{') {
-        putchar('{');
-        p++;
-      } else {
-        const char *end = strchr(p, '}');
-        if (end) {
-          if (*(end - 1) >= '0' && *(end - 1) <= '9') {
-            int index = *(end - 1) - '0';
-            va_list args_copy;
-            va_copy(args_copy, args);
+static FormatSpec decay_parse_spec(const char *spec) {
+  FormatSpec result = {FMT_INVALID, -1};
+  const char *p = spec;
 
-            for (int i = 0; i < index; i++) {
-              va_arg(args_copy, void *);
-            }
-            char *str = va_arg(args_copy, char *);
+  switch (*p++) {
+  case 'i':
+    if (strncmp(p, "8", 1) == 0) {
+      result.type = FMT_I8;
+      p += 1;
+    } else if (strncmp(p, "16", 2) == 0) {
+      result.type = FMT_I16;
+      p += 2;
+    } else if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_I32;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_I64;
+      p += 2;
+    }
+    break;
 
-            print_string(str);
-            va_end(args_copy);
-            p = end + 1;
-          } else {
-            char specifier = *(end - 1);
-            if (specifier == 's' || specifier == 'd' || specifier == 'x' ||
-                specifier == 'b' || specifier == 'c' || specifier == 'p' ||
-                specifier == 'f') {
-              // Handle valid format specifier
-              switch (specifier) {
-              case 's': {
-                char *str = va_arg(args, char *);
-                print_string(str);
-                break;
-              }
-              case 'd': {
-                int num = va_arg(args, int);
-                print_int(num);
-                break;
-              }
-              case 'x': {
-                int num = va_arg(args, int);
-                print_hex(num);
-                break;
-              }
-              case 'b': {
-                int num = va_arg(args, int);
-                print_binary(num);
-                break;
-              }
-              case 'c': {
-                int ch = va_arg(args, int);
-                putchar(ch);
-                break;
-              }
-              case 'p': {
-                void *ptr = va_arg(args, void *);
-                print_pointer(ptr);
-                break;
-              }
-              case 'f': {
-                double num = va_arg(args, double);
-                printf("%.*f", 2, num);
-                break;
-              }
-              }
-              p = end + 1;
-            } else {
-              // Print as normal literal
-              while (start <= end) {
-                putchar(*start++);
-              }
-              p = end + 1;
-            }
-          }
-        } else {
-          putchar('{');
-        }
-      }
-    } else if (*p == '}') {
-      if (*(p + 1) == '}') {
-        putchar('}');
-        p += 2;
-      } else {
-        putchar('}');
-        p++;
-      }
+  case 'u':
+    if (strncmp(p, "8", 1) == 0) {
+      result.type = FMT_U8;
+      p += 1;
+    } else if (strncmp(p, "16", 2) == 0) {
+      result.type = FMT_U16;
+      p += 2;
+    } else if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_U32;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_U64;
+      p += 2;
+    }
+    break;
+
+  case 'f':
+    if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_F32;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_F64;
+      p += 2;
+    } else if (strncmp(p, "128", 3) == 0) {
+      result.type = FMT_F128;
+      p += 3;
+    }
+    break;
+
+  case 's':
+    result.type = FMT_STRING;
+    break;
+  case 'p':
+    result.type = FMT_PTR_LOWER;
+    break;
+  case 'P':
+    result.type = FMT_PTR_UPPER;
+    break;
+  case 'x':
+    if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_HEX32_LOWER;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_HEX64_LOWER;
+      p += 2;
     } else {
-      putchar(*p++);
+      result.type = FMT_HEX32_LOWER;
+    }
+    break;
+  case 'X':
+    if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_HEX32_UPPER;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_HEX64_UPPER;
+      p += 2;
+    } else {
+      result.type = FMT_HEX32_UPPER;
+    }
+    break;
+  case 'b':
+    if (strncmp(p, "32", 2) == 0) {
+      result.type = FMT_BIN32;
+      p += 2;
+    } else if (strncmp(p, "64", 2) == 0) {
+      result.type = FMT_BIN64;
+      p += 2;
+    } else {
+      result.type = FMT_BIN32;
+    }
+    break;
+  default:
+    return result;
+  }
+
+  if (*p == '.') {
+    result.precision = 0;
+    while (*++p >= '0' && *p <= '9') {
+      result.precision = result.precision * 10 + (*p - '0');
     }
   }
 
+  // Check if we consumed entire specifier
+  return *p ? (FormatSpec){FMT_INVALID, -1} : result;
+}
+
+static void decay_format_value(char **buf, size_t *rem, FormatSpec spec,
+                               va_list *args) {
+  char num[256];
+  size_t len = 0;
+  uintptr_t ptr_val;
+  unsigned long long ull_val;
+
+  switch (spec.type) {
+  // Signed integers
+  case FMT_I8:
+    len = snprintf(num, sizeof(num), "%hhd", va_arg(*args, int));
+    break;
+  case FMT_I16:
+    len = snprintf(num, sizeof(num), "%hd", va_arg(*args, int));
+    break;
+  case FMT_I32:
+    len = snprintf(num, sizeof(num), "%d", va_arg(*args, int));
+    break;
+  case FMT_I64:
+    len = snprintf(num, sizeof(num), "%lld", va_arg(*args, long long));
+    break;
+
+  // Unsigned integers
+  case FMT_U8:
+    len = snprintf(num, sizeof(num), "%hhu", va_arg(*args, unsigned));
+    break;
+  case FMT_U16:
+    len = snprintf(num, sizeof(num), "%hu", va_arg(*args, unsigned));
+    break;
+  case FMT_U32:
+    len = snprintf(num, sizeof(num), "%u", va_arg(*args, unsigned));
+    break;
+  case FMT_U64:
+    len = snprintf(num, sizeof(num), "%llu", va_arg(*args, unsigned long long));
+    break;
+
+  // Floating-point
+  case FMT_F32:
+    len = snprintf(num, sizeof(num), "%.*f",
+                   spec.precision >= 0 ? spec.precision : 6,
+                   va_arg(*args, double));
+    break;
+  case FMT_F64:
+    len = snprintf(num, sizeof(num), "%.*f",
+                   spec.precision >= 0 ? spec.precision : 15,
+                   va_arg(*args, double));
+    break;
+  case FMT_F128:
+    len = snprintf(num, sizeof(num), "%.*Lf",
+                   spec.precision >= 0 ? spec.precision : 18,
+                   va_arg(*args, long double));
+    break;
+
+  // Strings and pointers
+  case FMT_STRING: {
+    const char *s = va_arg(*args, const char *);
+    len = snprintf(num, sizeof(num), "%s", s ? s : "(null)");
+    break;
+  }
+  case FMT_PTR_LOWER:
+    ptr_val = (uintptr_t)va_arg(*args, void *);
+    len = snprintf(num, sizeof(num), "%" PRIxPTR, ptr_val);
+    break;
+  case FMT_PTR_UPPER:
+    ptr_val = (uintptr_t)va_arg(*args, void *);
+    len = snprintf(num, sizeof(num), "%" PRIXPTR, ptr_val);
+    break;
+
+  // Hexadecimal
+  case FMT_HEX32_LOWER:
+    len = snprintf(num, sizeof(num), "%08x", va_arg(*args, unsigned));
+    break;
+  case FMT_HEX32_UPPER:
+    len = snprintf(num, sizeof(num), "%08X", va_arg(*args, unsigned));
+    break;
+  case FMT_HEX64_LOWER:
+    ull_val = va_arg(*args, unsigned long long);
+    len = snprintf(num, sizeof(num), "%llx", ull_val);
+    break;
+  case FMT_HEX64_UPPER:
+    ull_val = va_arg(*args, unsigned long long);
+    len = snprintf(num, sizeof(num), "%llX", ull_val);
+    break;
+
+  // Binary
+  case FMT_BIN32: {
+    unsigned val = va_arg(*args, unsigned);
+    char *p = num;
+    for (int i = 31; i >= 0; i--) {
+      *p++ = (val >> i) & 1 ? '1' : '0';
+      if (i > 0 && i % 4 == 0)
+        *p++ = '_';
+    }
+    *p = '\0';
+    len = p - num;
+    break;
+  }
+  case FMT_BIN64: {
+    unsigned long long val = va_arg(*args, unsigned long long);
+    char *p = num;
+    for (int i = 63; i >= 0; i--) {
+      *p++ = (val >> i) & 1 ? '1' : '0';
+      if (i > 0 && i % 4 == 0)
+        *p++ = '_';
+    }
+    *p = '\0';
+    len = p - num;
+    break;
+  }
+  default:
+    len = snprintf(num, sizeof(num), "[INVALID]");
+    break;
+  }
+
+  if (len < *rem) {
+    memcpy(*buf, num, len);
+    *buf += len;
+    *rem -= len;
+  }
+}
+
+static void decay_vprint(FILE *stream, const char *fmt, va_list args) {
+  char buffer[DECAY_BUFFER_SIZE];
+  char *buf_ptr = buffer;
+  size_t remaining = sizeof(buffer);
+  const char *start = fmt;
+  va_list args_copy;
+  va_copy(args_copy, args);
+
+  while (*fmt) {
+    if (*fmt == '{') {
+      // Write preceding content
+      size_t seg_len = fmt - start;
+      if (seg_len > 0) {
+        if (seg_len > remaining)
+          seg_len = remaining;
+        memcpy(buf_ptr, start, seg_len);
+        buf_ptr += seg_len;
+        remaining -= seg_len;
+      }
+
+      const char *spec_start = ++fmt;
+      while (*fmt && *fmt != '}')
+        fmt++;
+
+      if (*fmt != '}') {
+        memcpy(buf_ptr, "{", 1);
+        buf_ptr += 1;
+        remaining -= 1;
+        start = spec_start;
+        continue;
+      }
+
+      // Extract specifier
+      char spec_buf[DECAY_MAX_SPEC_LEN + 1];
+      size_t spec_len = fmt - spec_start;
+      if (spec_len > DECAY_MAX_SPEC_LEN)
+        spec_len = DECAY_MAX_SPEC_LEN;
+      memcpy(spec_buf, spec_start, spec_len);
+      spec_buf[spec_len] = '\0';
+
+      // Parse specifier
+      FormatSpec spec = decay_parse_spec(spec_buf);
+      if (spec.type == FMT_INVALID) {
+        // Invalid specifier - print original text
+        memcpy(buf_ptr, "{", 1);
+        buf_ptr += 1;
+        remaining -= 1;
+        if (spec_len > remaining)
+          spec_len = remaining;
+        memcpy(buf_ptr, spec_buf, spec_len);
+        buf_ptr += spec_len;
+        remaining -= spec_len;
+        memcpy(buf_ptr, "}", 1);
+        buf_ptr += 1;
+        remaining -= 1;
+      } else {
+        // Valid specifier - format value
+        decay_format_value(&buf_ptr, &remaining, spec, &args_copy);
+      }
+
+      start = ++fmt;
+    } else {
+      fmt++;
+    }
+  }
+
+  // Write remaining content
+  size_t final_len = fmt - start;
+  if (final_len > 0) {
+    if (final_len > remaining)
+      final_len = remaining;
+    memcpy(buf_ptr, start, final_len);
+    buf_ptr += final_len;
+  }
+
+  fwrite(buffer, 1, buf_ptr - buffer, stream);
+  va_end(args_copy);
+}
+
+/*==============================================================================
+  Public Interface
+==============================================================================*/
+
+/**
+ * Print formatted text to stdout
+ * @param fmt Format string using {spec} placeholders
+ */
+void print(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  decay_vprint(stdout, fmt, args);
   va_end(args);
-  putchar('\n');
 }
 
-inline void print_string(const char *str) {
-  if (str) {
-    printf("%s", str);
-  } else {
-    printf("(null)");
-  }
+/**
+ * Print formatted text to stdout with newline
+ * @param fmt Format string using {spec} placeholders
+ */
+void println(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  decay_vprint(stdout, fmt, args);
+  fputc('\n', stdout);
+  va_end(args);
 }
 
-inline void print_int(int value) { printf("%d", value); }
-
-inline void print_float(double value, int precision) {
-  printf("%.*f", precision, value);
+/**
+ * Print formatted text to specified stream
+ * @param stream Output stream (e.g. stdout, stderr)
+ * @param fmt Format string using {spec} placeholders
+ */
+void fprint(FILE *stream, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  decay_vprint(stream, fmt, args);
+  va_end(args);
 }
 
-inline void print_hex(int value) { printf("0x%x", value); }
-
-inline void print_binary(int value) {
-  printf("0b");
-  while (value) {
-    putchar((value & 1) + '0');
-    value >>= 1;
-  }
+/**
+ * Print formatted text to specified stream with newline
+ * @param stream Output stream (e.g. stdout, stderr)
+ * @param fmt Format string using {spec} placeholders
+ */
+void fprintln(FILE *stream, const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  decay_vprint(stream, fmt, args);
+  fputc('\n', stream);
+  va_end(args);
 }
-
-inline void print_pointer(void *ptr) { printf("%p", ptr); }
 
 #endif // DECAY_H
--- main.c
+++ main.c
@@ -1,30 +1,96 @@
 #include "decay.h"
-
-typedef struct {
-  int x;
-  int y;
-} Point2D;
+#include <stdio.h>
 
 int main() {
-  println("Hello {s}, you are {f} years old.", "User", 30.5627);
-  println("Decimal: {d}", 255);
-  println("Hexadecimal: {x}", 255);
-  println("Binary: {b}", 255);
-  println("String: {s}", "Hello, World!");
-  println("Character: {c}", 'A');
-  println("NULL string: {s}", NULL);
-  int x = 10;
-  println("Pointer to number {d}: {p}", x, &x);
-  println("Point: {{x: {d}, y: {d}}}", 10, 20);
-  println("{}");
-  println("{");
-  println("{{");
-  println("}");
-  println("}}");
+  i32 sixty_nine = 69;
+  i32 large = 987654321;
+  i32 max_i32 = I32_MAX;
+  i32 min_i32 = I32_MIN;
+  u32 max_u32 = U32_MAX;
 
+  f32 pi = 3.1415f;
+  f64 e = 2.718281828459045;
+  char *world = "World!";
+
+  // Basic string printing
+  print("{s}", "Hello ");
+  println("{s}", world);
   println("");
 
-  println("Countdown: {5} {4} {3} {2} {1} {0} Boom!", "Zero", "One", "Two",
-          "Three", "Four", "Five");
+  // Mixed types
+  println("Mixed types:  \n\
+            i32: {i32}   \n\
+            f64: {f64.5} \n\
+            String: {s}  \n\
+            u32: {u32}   \n\
+",
+          -42, 3.1415926535, "Decay", 4294967295U);
+  println("");
+
+  // Integer printing
+  println("+69:              \t{i32}", sixty_nine);
+  println("-69:              \t{i32}", -sixty_nine);
+  println("");
+
+  // Floating-point printing
+  println("f32 default:      \t{f32}", pi);
+  println("f32 precision(3): \t{f32.3}", pi);
+  println("f64 default:      \t{f64}", e);
+  println("f64 precision(9): \t{f64.9}", e);
+  println("");
+
+  // Pointer printing
+  void *ptr = &sixty_nine;
+  println("Pointer lower:    \t{p}", ptr);
+  println("Pointer upper:    \t{P}", ptr);
+  printf("System printf:    \t%p\n", ptr);
+  println("");
+
+  // Hexadecimal printing
+  println("x32:              \t{x32}", large);
+  println("X64:              \t{X64}", (u64)large);
+  printf("System hex:       \t%x\n", large);
+  println("");
+
+  // Binary printing
+  println("b32:              \t{b32}", large);
+  println("b64:              \t{b64}", (u64)large);
+  println("");
+
+  // Edge cases
+  println("Edge cases:");
+  println("Empty string:     \t\"{s}\"", "");
+  println("Null string:      \t\"{s}\"", NULL);
+  println("Braces:           \t{{i32}} {i32}", 42);
+  println("Braces:           \t{{");
+  println("Braces:           \t}}");
+
+  println("Malformed:        \t{i32", 42);
+  println("Malformed:        \ti32}", 42);
+  println("");
+
+  // Type limits
+  println("Type Limits:");
+  print("i8: ");
+  println("<{i8}, {i8}>", I8_MIN, I8_MAX);
+  print("i16: ");
+  println("<{i16}, {i16}>", I16_MIN, I16_MAX);
+  print("i32: ");
+  println("<{i32}, {i32}>", I32_MIN, I32_MAX);
+  print("i64: ");
+  println("<{i64}, {i64}>", I64_MIN, I64_MAX);
+  println("");
+
+  print("u8: ");
+  println("<{u8}, {u8}>", U8_MIN, U8_MAX);
+  print("u16 ");
+  println("<{u16}, {u16}>", U16_MIN, U16_MAX);
+  print("u32 ");
+  println("<{u32}, {u32}>", U32_MIN, U32_MAX);
+  print("u64 ");
+  println("<{u64}, {u64}>", U64_MIN, U64_MAX);
+  println("");
+
+  println("Testing succesfull.");
   return 0;
 }
